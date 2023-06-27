af6e5c48f1ce79c4be20c37b45393a42dbd4fcc9
Manuel Nieto <manuel.nieto@outlook.com>
2023-06-26 19:16:51 -0500

    v1.1 Domain Join status.

--- Detect_BitlockerKeyBackup.ps1
+++ Detect_BitlockerKeyBackup.ps1
@@ -1,3 +1,59 @@
+#region Functions
+function Test-DeviceJoinStatus {
+    <#
+    .SYNOPSIS
+        Verifies the Azure AD and Domain join status of a device.
+
+    .DESCRIPTION
+        The function checks if the device is joined to Azure AD and/or a traditional AD domain.
+
+    .NOTES
+        The function returns a custom object with properties StatusCode and Summary. StatusCode is 0 if the device is Azure AD Joined, 1 if not. 
+        Summary provides a summary string of the device's join status, including the hostname.
+        - Function was added to get more detail on the device. For Main function, if device is not AADJ, then there is no way to backup Bitlocker keys to AADJ.
+          So, in main script, if device is not AADJ then document details, but do Exit 0, since Fix becomes irrelevant, and a fail is just noise.
+    #>
+    [CmdletBinding()]
+    param (
+    )
+
+    try {
+        # Capture the output of the dsregcmd command, specifically looking for lines containing "AzureAdJoined" or "DomainJoined"
+        $result = dsregcmd /status | findstr /i "AzureAdJoined DomainJoined"
+
+        # If the command fails to execute, an exception is thrown
+        if ($null -eq $result) {
+            throw "Failed to execute dsregcmd command."
+        }
+
+        # Check the result for "AzureAdJoined : YES" - if it exists, the device is Azure AD joined
+        $AzureAdJoined = if($result -match "AzureAdJoined : YES"){"Yes"} else {"No"}
+
+        # Similarly, check the result for "DomainJoined : YES" - if it exists, the device is Domain joined
+        $DomainJoined = if($result -match "DomainJoined : YES"){"Yes"} else {"No"}
+
+        # Retrieve the hostname of the device
+        $hostname = $env:COMPUTERNAME
+
+        # Compile the findings into a summary string, now including the hostname
+        $summary = "Hostname = $hostname - AADJ = $AzureAdJoined, ADJ = $DomainJoined. "
+
+        # Define status code based on Azure AD join status
+        $statusCode = if ($AzureAdJoined -eq "Yes") { 0 } else { 1 }
+
+        # Return a custom object with properties StatusCode and Summary
+        return New-Object PSObject -Property @{
+            StatusCode = $statusCode
+            Summary = $summary
+        }
+    }
+    catch {
+        # Catch any errors that occurred during execution
+        Write-Host "An error occurred: $_" -ForegroundColor Red
+    }
+}
+
+
 function Test-AzureADBitLockerBackup {
     <#
     .SYNOPSIS
@@ -187,27 +243,63 @@ function Get-BitLockerVolumeInfo {
     }
 }
 
+#endregion Functions
+
 Write-Host "`n`n"
+
+#region Main
+
+$txtStatus = ""
 
 # Call functions
-$encryptedDrives = Test-AllDrivesEncryption
-$bitlockerinfo = Get-BitLockerVolumeInfo
-$bitlockerOS = Test-AzureADBitLockerBackup
 
-$txtStatus = "$encryptedDrives[$bitlockerinfo]"
+$adJoined = Test-DeviceJoinStatus               
+$txtStatus += "$($adJoined.Summary)"                        #Document domain join info. No matter the result, we want to upload info to Intune.
+
+# Is device Azure AD Joined? if not, there's nothing to do here, can't backup bitlocker keys to AAD. Document drive status, send info to Intune.
+if ($($adJoined.StatusCode) -eq 0) {
+    $bitlockerBackupStatus = Test-AzureADBitLockerBackup -nDays 7     #Bitlocker keys have been sucessfully backed up to Azure AD?
+}
+else {
+    $bitlockerBackupStatus = -2
+}
+$encryptedDrives = Test-AllDrivesEncryption                 #Check encryption status of device drives.
+$bitlockerinfo = Get-BitLockerVolumeInfo                    #Get Bitlocker info for devices drives, info will get uploaded to Intune Remediation status.
+
+
+# Build summary text of findings and status.
+$txtStatus += "$encryptedDrives"
+if ($bitlockerinfo -ne "") {
+    $txtStatus += "[$bitlockerinfo]"                                #Add bitlocker detailed info, only if we were able to get it.
+}
+
 
 Write-Host "`n`n"
 
-#Return result
-if ($bitlockerOS -eq 0) {
+#Return result. Last line of script is automatically uploaded to Intune, can be seen in the "Output" columns of Remediation Device Status.
+# If the BitLocker key has been successfully backed up to Azure AD ($bitlockerBackupStatus equals 0),
+# it prints an OK message along with the current date and time, and the status text stored in $txtStatus.
+# Then it exits the script with a success status code (0).
+if ($bitlockerBackupStatus -eq 0) {
     Write-Host "OK $([datetime]::Now) : $txtStatus"
     Exit 0
 }
-elseif ($bitlockerOS -eq 1) {
-    Write-Host "WARNING $([datetime]::Now) : $txtStatus"
+
+# If the BitLocker key backup to Azure AD has failed ($bitlockerBackupStatus equals 1),
+# it prints a FAIL message along with the current date and time, and the status text stored in $txtStatus.
+# Then it exits the script with an error status code (1). This will call the Remediation script.
+elseif ($bitlockerBackupStatus -eq 1) {
+    Write-Host "FAIL $([datetime]::Now) : $txtStatus"
     Exit 1
 }
+
+# If the value of $bitlockerBackupStatus is anything other than 0 or 1,
+# it prints a WARNING message along with the current date and time, and the status text stored in $txtStatus.
+# Then it exits the script with a success status code (0). No need to execute Remediation script, 
+#   but something is wrong, something that can't be fix with Remediation script.
 else {
-    Write-Host "NOTE $([datetime]::Now) : $txtStatus"
+    Write-Host "WARNING $([datetime]::Now) : $txtStatus"
     Exit 0
 }
+
+#endregion Main
